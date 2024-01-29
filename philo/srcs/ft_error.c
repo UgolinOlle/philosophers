94041a3bc137fcc1b825d45cbaac2ecc58e6354b
Ugolin Ollé <hello@ugolin-olle.com>
2024-01-29 22:01:59 +0700

    feat: add error handler

--- philo/srcs/ft_error.c
+++ philo/srcs/ft_error.c
@@ -13,23 +13,50 @@
 #include "../includes/philosophers.h"
 
 /**
- * @brief Exit program with error message and free all allocated memory.
+ * @brief Free all allocated memory.
  *
- * @param content str - Error message.
- * @param philo t_philo - Struct with all data.
+ * @param *philo t_philo - Pointer to the philo struct.
+ * @return void
  */
-void ft_error(char *content, t_philo *philo) {
-  int i;
+void ft_free(t_philo *philo) {
+  unsigned int i;
 
   if (!philo->data)
-    return (NULL);
+    return;
   if (philo->data->fork_locked)
     free(philo->data->fork_locked);
-  if (philo != NULL) {
-    i = 0;
-    while (i < philo->data->n_philo) {
-      free(philo);
-      i++;
-    }
+  if (philo) {
+    free(philo);
   }
+  return;
+}
+
+/**
+ * @brief Destroy all mutexes.
+ *
+ * @param *philo t_philo - Pointer to the philo struct.
+ * @return void
+ */
+void ft_destroy_mutex(t_philo *philo) {
+  unsigned int i;
+
+  i = 0;
+  while (i < philo->data->n_philo) {
+    pthread_mutex_destroy(&philo->data->fork_locked[i]);
+    i++;
+  }
+  return;
+}
+
+/**
+ * @brief Exit the program with a message.
+ *
+ * @param *philo t_philo - Pointer to the philo struct.
+ * @param *msg char - Message to print.
+ * @return void
+ */
+void ft_exit(t_philo *philo, char *msg) {
+  ft_free(philo);
+  printf("%s\n", msg);
+  exit(1);
 }
