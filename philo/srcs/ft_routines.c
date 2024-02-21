efa72c65925512086646e6127e7285955d13d9b8
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-21 20:54:18 +0700

    fix(core): return pointer du to thread

--- philo/srcs/ft_routines.c
+++ philo/srcs/ft_routines.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_routine.c                                       :+:      :+:    :+:   */
+/*   ft_routines.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 18:54:54 by ugolin-olle       #+#    #+#             */
-/*   Updated: 2024/02/21 19:13:10 by ugolin-olle      ###   ########.fr       */
+/*   Updated: 2024/02/21 20:52:47 by ugolin-olle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@
  * @param void *arg - Philosopher structure.
  * @return void
  */
-static void	ft_alone_routine(void *arg)
+static void	*ft_alone_routine(void *arg)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
 	ft_action_sleep(philo->global->tt_die);
 	ft_status(philo, "died");
+	return (NULL);
 }
 
 /**
@@ -38,6 +39,7 @@ void	ft_alone(t_global *global)
 	global->t_start = ft_get_time();
 	pthread_create(&global->philos[0]->thread, NULL, &ft_alone_routine,
 		global->philos[0]);
+	return (1);
 }
 
 /**
