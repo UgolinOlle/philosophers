98749d10bb4a864ba342931d16d6d65878d8841c
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-25 14:48:04 +0700

    fix(core): format due to norminette
    c

--- philo/srcs/ft_actions_utils.c
+++ philo/srcs/ft_actions_utils.c
@@ -6,7 +6,7 @@
 /*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 12:58:58 by ugolin-olle       #+#    #+#             */
-/*   Updated: 2024/02/25 13:00:00 by ugolin-olle      ###   ########.fr       */
+/*   Updated: 2024/02/25 13:02:15 by ugolin-olle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
  *
  * @param t_philo *philo - Philosopher structure.
  * @return void
-*/
+ */
 void	ft_lock_forks(t_philo *philo)
 {
-    pthread_mutex_lock(&philo->global->forks_mutex[philo->left_fork]);
-    pthread_mutex_lock(&philo->global->forks_mutex[philo->right_fork]);
+	pthread_mutex_lock(&philo->global->forks_mutex[philo->left_fork]);
+	pthread_mutex_lock(&philo->global->forks_mutex[philo->right_fork]);
 }
 
 /**
@@ -29,9 +29,9 @@ void	ft_lock_forks(t_philo *philo)
  *
  * @param t_philo *philo - Philosopher structure.
  * @return void
-*/
+ */
 void	ft_unlock_forks(t_philo *philo)
 {
-    pthread_mutex_unlock(&philo->global->forks_mutex[philo->left_fork]);
-    pthread_mutex_unlock(&philo->global->forks_mutex[philo->right_fork]);
+	pthread_mutex_unlock(&philo->global->forks_mutex[philo->left_fork]);
+	pthread_mutex_unlock(&philo->global->forks_mutex[philo->right_fork]);
 }
